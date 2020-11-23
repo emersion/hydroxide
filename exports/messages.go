b116e0d5b0a8868599584266e53c77a5e93fbeb7
Simon Ser <contact@emersion.fr>
2020-11-23 10:02:20 +0100

    exports: set charset to UTF-8
    Same as fdc51440c399 ("imap: set charset header to utf-8") but for
    exports.

--- exports/messages.go
+++ exports/messages.go
@@ -1,9 +1,9 @@
 package exports
 
 import (
+	"bufio"
 	"fmt"
 	"io"
-	"bufio"
 	"strings"
 
 	"github.com/emersion/go-mbox"
@@ -28,7 +28,7 @@ func writeMessage(c *protonmail.Client, privateKeys openpgp.KeyRing, w io.Writer
 	}
 
 	mh := mail.Header{message.Header{th}}
-	mh.SetContentType(mimeType, nil)
+	mh.SetContentType(mimeType, map[string]string{"charset", "utf-8"})
 	mh.Set("Content-Transfer-Encoding", "quoted-printable")
 
 	// TODO: add support for attachments
