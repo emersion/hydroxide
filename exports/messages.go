emersion/hydroxide

commit b116e0d5b0a8868599584266e53c77a5e93fbeb7

exports: set charset to UTF-8
Same as fdc51440c399 ("imap: set charset header to utf-8") but for
exports.

exports/messages.go
@@ -1,9 +1,9 @@
 package exports
 
 import (
+	"bufio"
 	"fmt"
 	"io"
-	"bufio"
 	"strings"
 
 	"github.com/emersion/go-mbox"
@@ -28,7 +28,7 @@ func writeMessage(c *protonmail.Client, privateKeys openpgp.KeyRing, w io.Writer
 	}
 
 	mh := mail.Header{message.Header{th}}
-	mh.SetContentType(mimeType, nil)
+	mh.SetContentType(mimeType, map[string]string{"charset", "utf-8"})
 	mh.Set("Content-Transfer-Encoding", "quoted-printable")
 
 	// TODO: add support for attachments

commit 358ca10548fbac7f695d590dfa6882675b48b757

exports: fix typo
Fixes: b116e0d5b0a8 ("exports: set charset to UTF-8")

exports/messages.go
@@ -28,7 +28,7 @@ func writeMessage(c *protonmail.Client, privateKeys openpgp.KeyRing, w io.Writer
 	}
 
 	mh := mail.Header{message.Header{th}}
-	mh.SetContentType(mimeType, map[string]string{"charset", "utf-8"})
+	mh.SetContentType(mimeType, map[string]string{"charset": "utf-8"})
 	mh.Set("Content-Transfer-Encoding", "quoted-printable")
 
 	// TODO: add support for attachments