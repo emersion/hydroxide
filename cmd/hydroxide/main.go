1e85c15ffc36a322e54b472732b3f8c5fbb19e39
Simon Ser <contact@emersion.fr>
2023-04-04 17:04:03 +0200

    Bump app version
    Closes: https://github.com/emersion/hydroxide/issues/248

--- cmd/hydroxide/main.go
+++ cmd/hydroxide/main.go
@@ -39,7 +39,7 @@ var (
 func newClient() *protonmail.Client {
 	return &protonmail.Client{
 		RootURL:    apiEndpoint,
-		AppVersion: "web-mail@5.0.17.9",
+		AppVersion: "web-mail@5.0.19.5",
 		Debug:      debug,
 	}
 }
